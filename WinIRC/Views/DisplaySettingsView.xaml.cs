198c022a40b00ad76aaa6b2bb2d48f6e0a0b7ff3
rymate1234 <supermarioryan@gmail.com>
2016-05-22 13:51:54 +0100

    WinIRC 1.2.5

--- WinIRC/Views/DisplaySettingsView.xaml.cs
+++ WinIRC/Views/DisplaySettingsView.xaml.cs
@@ -22,7 +22,7 @@ namespace WinIRC
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DisplaySettingsView
+    public sealed partial class DisplaySettingsView : BaseSettingsPage
     {
         private bool SettingsLoaded;
 
