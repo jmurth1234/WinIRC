811eb2718aad664eaa1868f3cfd11ba3313e16f8
Ryan <supermarioryan@gmail.com>
2019-08-17 21:36:45 +0100

    Remove accidental second render

--- src/index.tsx
+++ src/index.tsx
@@ -45,11 +45,6 @@ mergeStyles({
   }
 });
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
 const rootElement = document.getElementById("root");
 if (rootElement && rootElement.hasChildNodes()) {
   ReactDOM.hydrate(<App />, rootElement);
