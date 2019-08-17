fc85ab6e63e018227d84e99fe8d1c8cc8596fa7a
Ryan <supermarioryan@gmail.com>
2019-08-17 19:15:26 +0100

    Updates

---
+++ precache-manifest.a2c155c95605f8b7b4b75ad36e33f3ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bf7e907f06ea55af491f9668f234b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba43692508cce320a4b",
+    "url": "/static/js/2.f6fe0ac7.chunk.js"
+  },
+  {
+    "revision": "cfc04b3212816344b810",
+    "url": "/static/js/main.6e58a869.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
