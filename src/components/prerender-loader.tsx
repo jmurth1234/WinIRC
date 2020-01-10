655a830775516130c4fba7ceb6c3b4e03dbece42
rymate1234 <ryan.murthick@gmail.com>
2020-01-10 19:37:39 +0000

    Properly fixed build

--- src/components/prerender-loader.tsx
+++ src/components/prerender-loader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import loadable from "@loadable/component";
 import { PrerenderedComponent } from "react-prerendered-component";
 
-export const prerenderedLoader = dynamicImport => {
+export const prerenderedLoader = (dynamicImport: any) => {
   const LoadableComponent = loadable(dynamicImport);
   return React.memo(props => (
     // you can use the `.preload()` method from react-loadable or react-imported-component`
